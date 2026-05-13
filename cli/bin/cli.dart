97fa89e28bac77a83ed0c1e44db256921045a52f
GerliRand <gerli@hotmail.fi>
2026-05-13 22:24:05 +0300

    Add async Wikipedia API functionality (Write asynchronous code)

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
@@ -10,33 +11,58 @@ void main(List<String> arguments) {
   // dart run .\bin\cli.dart version
   else if (arguments.first == 'version') {
     print('Current version is: $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     // jos listan (item) määrä on suurempi kuin 1, silloin tehdään sublist
     // muuten asetetaan null inputArgs:in
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
+    searchWikipedia(inputArgs); // ei tarvita tässä await:ia
   } else {
     printUsage();
   }
 }
 
 // ? - tietotyyppi on joko lista tai null
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async {
   // final - lisätään kerran muuttuja jota ei pysty myöhemmin muutamaan
   final String articleTitle;
 
   // jos arguments on null tai tyhjä, silloin kysytään title ja tulostetaan se
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title: ');
-    articleTitle = stdin.readLineSync() ?? '';
+
+    final inputFromStdin = stdin.readLineSync(); // luetaan teksti
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting');
+      return; // jos ei ole tekstiä niin lopetetaan
+    }
+
+    articleTitle = inputFromStdin;
   } else {
     articleTitle = arguments.join(' ');
   }
-  print('Current article title: $articleTitle');
+  print('Looking up articles about "$articleTitle". Please wait...');
+  // Kutsu API:a ja odota tulosta
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
 }
 
 void printUsage() {
   print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+    "The following commands are valid: 'help', 'version', 'wikipedia <ARTICLE-TITLE>'",
   );
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org', // Wikipedia API domain
+    '/api/rest_v1/page/summary/$articleTitle', // API path for article summary
+  );
+
+  final response = await http.get(url); // Make the HTTP request
+
+  if (response.statusCode == 200) {
+    return response.body; // Return the response body if successful
+  }
+  // Return an error message if the request failed
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
